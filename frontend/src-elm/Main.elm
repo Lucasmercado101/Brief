669fe97a1785a60062556a83765e706a1a0247d1
Lucasmercado101 <lucasmercado101@gmail.com>
2023-09-04 20:22:47 -0300

    can see notes

--- frontend/src-elm/Main.elm
+++ frontend/src-elm/Main.elm
@@ -3,6 +3,7 @@ module Main exposing (..)
 import Browser
 import Cmd.Extra exposing (pure)
 import Html exposing (Html, div, text)
+import Html.Attributes exposing (style)
 
 
 
@@ -18,8 +19,20 @@ subscriptions _ =
 -- MODEL
 
 
+type Either a b
+    = LeftType a
+    | RightType b
+
+
+type alias Note =
+    { title : String
+    , content : Either String (List String)
+    }
+
+
 type alias Model =
-    {}
+    { notes : List Note
+    }
 
 
 
@@ -36,7 +49,17 @@ type Msg
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( {}, Cmd.none )
+    ( { notes =
+            [ { title = "My first note"
+              , content = LeftType "This is my first note"
+              }
+            , { title = "My second note"
+              , content = LeftType "Second note here"
+              }
+            ]
+      }
+    , Cmd.none
+    )
 
 
 main : Program () Model Msg
@@ -64,6 +87,30 @@ update msg model =
 -- VIEW
 
 
-view : model -> Html msg
+view : Model -> Html msg
 view model =
-    div [] [ text "Hello, World!" ]
+    div []
+        (List.map
+            (\note ->
+                div
+                    [ style "border" "1px solid black"
+                    , style "margin" "10px"
+                    , style "padding" "10px"
+                    , style "display" "flex"
+                    , style "flex-direction" "column"
+                    ]
+                    [ div [] [ text note.title ]
+                    , div []
+                        [ text
+                            (case note.content of
+                                LeftType s ->
+                                    s
+
+                                RightType l ->
+                                    String.join "\n" l
+                            )
+                        ]
+                    ]
+            )
+            model.notes
+        )
