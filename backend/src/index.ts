fffb412687a559b24625dfc43e693e1752430468
Lucasmercado101 <lucasmercado101@gmail.com>
2023-09-22 18:14:55 -0300

    added prisma

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,12 @@
 import { Elysia } from "elysia";
+import { Prisma, PrismaClient } from "@prisma/client";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const prisma = new PrismaClient();
+
+const PORT = process.env.PORT || 4000;
+
+const app = new Elysia().get("/", () => "Hello Elysia").listen(PORT);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port} on port ${PORT}`
 );
