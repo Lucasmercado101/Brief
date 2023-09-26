9d30b0b1f4bdcf15649e2d31bef498af18954d40
Lucasmercado101 <lucasmercado101@gmail.com>
2023-09-26 19:05:32 -0300

    changed some code

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,4 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
 import { PrismaClient } from "@prisma/client";
 import { logger } from "./logger";
 
@@ -6,19 +6,35 @@ const prisma = new PrismaClient();
 
 const PORT = process.env.PORT || 4000;
 
-const app = new Elysia()
+new Elysia({
+  cookie: {
+    secrets: process.env.COOKIE_SECRETS || "Fischl von Luftschloss Narfidort",
+    sign: ["user"]
+  }
+})
   .use(logger())
-  .post("/register", async () => {
-    return await prisma.user.create({
-      data: {
-        email: "test@gmail.com",
-        password: "test"
-      }
-    });
+  .post(
+    "/register",
+    async () => {
+      return await prisma.user.create({
+        data: {
+          email: "test@gmail.com",
+          password: Bun.password.hashSync("test")
+        }
+      });
+    },
+    {
+      body: t.Object({
+        email: t.String({ format: "email" }),
+        password: t.String()
+      })
+    }
+  )
+  .post("/login", async ({ cookie: { user } }) => {
+    user.value = { something: "abcasd" };
+    return "logged in";
   })
   .get("/", () => "Hello Elysia")
-  .listen(PORT);
-
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port} `
-);
+  .listen(PORT, (server) => {
+    console.log(`🦊 Elysia is running at ${server?.hostname}:${server?.port} `);
+  });
