ebe8d857463569353c5d85a8204e6e2113b3f5a2
Lucasmercado101 <lucasmercado101@gmail.com>
2023-09-28 16:30:28 -0300

    checking if it has session cookie

--- frontend/public/main.js
+++ frontend/public/main.js
@@ -1,5 +1,11 @@
 "use strict";
 
+const cookieName = "session";
+// regex obtained from: https://stackoverflow.com/a/25617724
+const hasSessionCookie = document.cookie.match(
+  new RegExp(`^(.*;)?\s*${cookieName}\s*=\s*[^;]+(.*)?$`)
+);
+
 const app = Elm.Main.init({
   flags: { seeds: Array.from(self.crypto.getRandomValues(new Uint32Array(4))) },
   node: document.getElementById("root")
