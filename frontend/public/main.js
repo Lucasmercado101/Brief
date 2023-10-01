5d81d87817ffaf22693a76eebeb185637cc2f69d
Lucasmercado101 <lucasmercado101@gmail.com>
2023-09-30 23:23:14 -0300

    added typescript

--- frontend/public/main.js
+++ frontend/public/main.js
@@ -1,33 +1,25 @@
 "use strict";
-
 // DB
-
 if ("indexedDB" in self) {
-  const openReq = indexedDB.open("notes", 1);
-
-  openReq.onsuccess = function () {
-    console.log("open success");
-  };
+    const openReq = indexedDB.open("notes", 1);
+    openReq.onupgradeneeded = function (event) {
+        console.log("open success");
+        // const db = event.target.results;
+        // console.log(db.objectStoreNames.contains("notes"));
+    };
 }
-
 // DB end ------
-
 const cookieName = "session";
 // regex obtained from: https://stackoverflow.com/a/25617724
-const hasSessionCookie = document.cookie.match(
-  new RegExp(`^(.*;)?\s*${cookieName}\s*=\s*[^;]+(.*)?$`)
-);
-
+const hasSessionCookie = document.cookie.match(new RegExp(`^(.*;)?\s*${cookieName}\s*=\s*[^;]+(.*)?$`));
+// @ts-ignore
 const app = Elm.Main.init({
-  flags: {
-    seeds: Array.from(self.crypto.getRandomValues(new Uint32Array(4))),
-    hasSessionCookie: !!hasSessionCookie
-  },
-  node: document.getElementById("root")
+    flags: {
+        seeds: Array.from(self.crypto.getRandomValues(new Uint32Array(4))),
+        hasSessionCookie: !!hasSessionCookie
+    },
+    node: document.getElementById("root")
 });
-
 app.ports.requestRandomValues.subscribe(function () {
-  app.ports.receiveRandomValues.send(
-    Array.from(self.crypto.getRandomValues(new Uint32Array(4)))
-  );
+    app.ports.receiveRandomValues.send(Array.from(self.crypto.getRandomValues(new Uint32Array(4))));
 });
