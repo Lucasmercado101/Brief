5f4766cb53e28b76785a9a12462db93333023bda
Lucasmercado101 <lucasmercado101@gmail.com>
2023-10-25 20:22:30 -0300

    added LogIn to routes

--- frontend/src-elm/Route.elm
+++ frontend/src-elm/Route.elm
@@ -1,30 +1,32 @@
 module Route exposing (..)
 
 import Url exposing (Url)
-import Url.Parser exposing (Parser, map, oneOf, parse, top)
+import Url.Parser exposing (Parser, map, oneOf, parse, s, top)
 
 
 type Route
     = Home
+    | LogIn
 
 
 route : Parser (Route -> a) a
 route =
     oneOf
         [ map Home top
+        , map LogIn (s "log-in")
         ]
 
 
 fromUrl : Url -> Route
 fromUrl url =
-    parse route url |> Maybe.withDefault Home
+    parse route url |> Maybe.withDefault LogIn
 
 
 toRoute : String -> Route
 toRoute string =
     case Url.fromString string of
         Nothing ->
-            Home
+            LogIn
 
         Just url ->
-            Maybe.withDefault Home (parse route url)
+            Maybe.withDefault LogIn (parse route url)
